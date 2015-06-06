b21662fe8c0275dc1cd67dfe76e3bc7e7911fab6
Ryan Smith <ryans970@hotmail.com>
2015-06-05 19:09:57 -0700

    Reverted to prevous commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605233634) do
+ActiveRecord::Schema.define(version: 20150604061743) do
 
-# Could not dump table "products" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end

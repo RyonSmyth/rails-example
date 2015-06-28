37cde8406f6dfa05857758eba0b484ec784d2a3a
Ryan Smith <ryans970@hotmail.com>
2015-06-27 20:35:10 -0700

    Added Admin Controls

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-    devise_for :users, :skip => [:sessions]
-    as :user do
-    get 'signin' => 'devise/sessions#new', :as => :new_user_session
-    post 'signin' => 'devise/sessions#create', :as => :user_session
-    get '/users/sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
+  devise_for :users
   resources :products do
     resources :comments
   end

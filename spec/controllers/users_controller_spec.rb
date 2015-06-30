f3ae5adf9ee585f7c2594c3fb6b4a48bb666d8bc
Ryan Smith <ryans970@hotmail.com>
2015-06-29 22:40:47 -0700

    Added functional tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+  describe "GET #index" do
+    context "user is logged on" do
+      before do
+        @user = User.new
+        sign_in @user
+      end
+
+      it "responds successfully with an HTTP 200 status code" do
+        get :index
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+
+      it "renders the index template" do
+        get :index
+        expect(response).to render_template("index")
+      end
+    end
+  end
+end

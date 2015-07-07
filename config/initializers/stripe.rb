95c3f150414713dc3da61856b98a73301806aa52
Ryan Smith <ryans970@hotmail.com>
2015-07-06 23:12:43 -0700

    Fixed Errors

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,13 @@
-if Rails.env.production?
-  Rails.configuration.stripe = {
-    :publishable_key => 'pk_live_ieopulzJqkG4GxcSnPx0gRpl',
-    :secret_key      => 'sk_live_6ViWhvDZpY2k69pvnapTOECR'
-  }
-else
+# if Rails.env.production?
+#  Rails.configuration.stripe = {
+#    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+#    :secret_key      => ENV['STRIPE_SECRET_KEY']
+#  }
+# else
   Rails.configuration.stripe = {
     :publishable_key => 'pk_test_n1KMjfItMNfQcvP1pf1mSb4u',
     :secret_key      => 'sk_test_LwTNxIOrCOWBY2PyPSEw8U5R'
   }
-end
+# end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]

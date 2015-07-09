6b4b2d5b37f4f9ae6f7bd32744bd3c92826ce722
Ryan Smith <ryans970@hotmail.com>
2015-07-08 23:53:03 -0700

    Added AJAX

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 // = require jquery_ujs
 // = require turbolinks
 // = require_tree .
+
+$.ajax({
+  success: function(e,data, status, xhr){ alert('Success!')
+   },
+  error: function(e, xhr, status, error){ alert('Error!')
+   }
+}); 

39b0f0589ddb04756da07394b9cc6dc3f115b69c
Ryan Smith <ryans970@hotmail.com>
2015-07-11 18:41:24 -0700

    Added automatic mailer for new users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,11 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
+ def welcome_email(user)
+   @user = user
+   mail(:to => user.email, :subject => "Welcome to Berlin Bikes")
+ end
+
  def contact_form(email, name, message)
   @message = message
   mail(:from => email,
